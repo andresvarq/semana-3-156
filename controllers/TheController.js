738c5c52e1f9bae4d39a21d4580267670d43c813
andresvarq <66755854+andresvarq@users.noreply.github.com>
2020-12-13 03:03:00 -0500

    ruteos

---
+++ controllers/TheController.js
@@ -0,0 +1,41 @@
+const models = require('../models');
+var bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.signin = async(req, res, next) => {
+    try {
+        const user = await models.User.findOne({where: {email: req.body.email}});
+        if(user) {
+            const validPassword = bcrypt.compareSync(req.body.password, user.password);
+            if(validPassword) {
+                const token = jwt.sign({
+                    id: user.id,
+                    user: user.username,
+                    email: user.email,
+                    rol: user.rol
+                }, 'config.secret', {
+                    expiresIn: 86400,
+                }
+                );
+                res.status(200).json({
+                    auth: true,
+                    tokenReturn: token,
+                    user: user
+                })
+            } else {
+                res.status(401).json({
+                    error: 'Que pasa papi, algo no está bien'
+                })
+            }
+        } else {
+            res.status(404).json({
+                error: 'Que pasa papi, algo no está bien'
+            })
+        }
+    } catch(error) {
+        res.status(505).send({
+            message: 'Paila'
+        }),
+        next();
+    }
+};
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express');
 const db = require('./models');
 const app = express()
 const bodyParser = require('body-parser');
+const apiRouter = require('./routes');
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -20,6 +21,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 /*se debe contar un una ruta por medio de método post para el inicio de sesión de la siguiente manera:
 '/api/auth/signin'
 */
+
+app.use('/api', apiRouter);
+
 app.get('/', function(req, res) {
     console.log("Estructura base del proyecto backend");
     res.send("Estructura base del proyecto backend");
