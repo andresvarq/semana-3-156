f18e26e1912d377f5955577bc58ee8cf72868fa4
andresvarq <66755854+andresvarq@users.noreply.github.com>
2020-12-13 02:40:56 -0500

    registro base de datos

--- seeders/20201114225211-seed-user.js
+++ seeders/20201114225211-seed-user.js
@@ -2,17 +2,17 @@
 
 module.exports = {
     up: async(queryInterface, Sequelize) => {
-        return queryInterface.bulkInsert('Users', [{
+        return queryInterface.bulkInsert('users', [{
             name: 'carlos',
             email: 'ejemplo@gmail.com',
-            password: '$2y$08$FTP/jKGNASwJf0ero7SBe.kQmUsOSjWYupPZ6/lS6en6RcithXFKO',
+            password: '$2y$10$yfQoFxHHpG7D9VraV91d7uDBUvVyyz4Yt/UhHeMbPZW6vUVU3a7t6 ',
             createdAt: new Date(),
             updatedAt: new Date()
         }]);
     },
 
     down: async(queryInterface, Sequelize) => {
-        return queryInterface.bulkDelete('Users', null, {});
+        return queryInterface.bulkDelete('users', null, {});
 
     }
 };
